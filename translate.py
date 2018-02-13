eb68a1af2393a7dbedf3fb3b50e6a8a49ab9c770
nwatts02 <nathan.watts@tufts.edu>
2018-02-13 14:10:39 -0500

    beginnings of tensorflow session

--- translate.py
+++ translate.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import os
 import string
 import re
+import numpy as np
 
 train_pos_dir = "data/aclImdb/train/pos/"
 train_neg_dir = "data/aclImdb/train/neg/"
@@ -25,26 +26,54 @@ def load_dir(path):
 				data.append(words)
 	return data
 
-print "loading vocabulary"
+print "loading vocabulary\r"
 vocab = open(vocab_path).readlines()
-word_to_int = {};
+vocab_inv = {} # Converts words to indexes
 for i in xrange(len(vocab)):
-	word_to_int[vocab[i]] = i
+	vocab_inv[vocab[i]] = i
 
 print len(vocab)
 
-print "loading data"
+print "loading data\r"
 
 train_pos = load_dir(train_pos_dir)
-print "loaded %i positive training examples"%len(train_pos)
+print "loaded %i positive training examples\r"%len(train_pos)
 
 train_neg = load_dir(train_neg_dir)
-print "loaded %i negative training examples"%len(train_neg)
+print "loaded %i negative training examples\r"%len(train_neg)
 
 test_pos = load_dir(test_pos_dir)
-print "loaded %i positive test examples"%len(test_pos)
+print "loaded %i positive test examples\r"%len(test_pos)
 
 test_neg = load_dir(test_neg_dir)
-print "loaded %i negative test examples"%len(test_neg)
+print "loaded %i negative test examples\r"%len(test_neg)
+
+#checkpoint_file = open("model.ckpt", "w")
+
+x_train = np.array([vocab_inv[word] for word in [sentence for sentence in (train_pos + train_neg)]])
+y_train = np.hstack(np.ones(len(train_pos)), -np.ones(len(train_neg)))
 
 
+batch_size = 128
+valid_freq = 1
+checkpoint_freq = 1
+
+sequence_length = x_train.shape[1]
+num_classes = y_train.shape[1]
+vocab_size = len(vocabulary)
+filter_sizes = map(int, '3,4,5'.split(','))
+validate_every = len(y_train) / (batch_size * valid_freq)
+checkpoint_every = len(y_train) / (batch_size * checkpoint_freq)
+
+device = '/gpu:0'
+sess = tf.InteractiveSession()
+
+with tf.device(device):
+    # Placeholders
+    data_in = tf.placeholder(tf.int32, [None, sequence_length], name='data_in')
+    data_out = tf.placeholder(tf.float32, [None, num_classes], name='data_out')
+    dropout_keep_prob = tf.placeholder(tf.float32, name='dropout_keep_prob')
+    # Stores the accuracy of the model for each batch of the validation testing
+    valid_accuracies = tf.placeholder(tf.float32)
+    # Stores the loss of the model for each batch of the validation testing
+    valid_losses = tf.placeholder(tf.float32)
