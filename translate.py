fixed training and added gradient retrieval

translate.py
@@ -15,6 +15,8 @@ tf.flags.DEFINE_string('load', None,
                        'Restore a model from the given path.')
 tf.flags.DEFINE_string('custom_input', '',
                        'Evaluate the model on the given string.')
+tf.flags.DEFINE_float('get_grads', 0,
+                       'Evaluate the model on the given string.')
 tf.flags.DEFINE_bool('skip_train', '',
                        'Skip training.')
 tf.flags.DEFINE_bool('gpu', False,
@@ -74,82 +76,22 @@ def load_dir(path, num_samples):
                         data.append(words)
                     if len(data) >= num_samples:
                         return data
+
     return data
 
-print "loading vocabulary\r"
-vocab = open(vocab_path).read().splitlines()
-
-
-# Add various important numbers and tokens
-vocab+= ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "<NUM>", "<UNK>", "<PAD>"]
-vocab_inv = {} # Converts words to indexes
-for i in xrange(len(vocab)):
-    vocab_inv[vocab[i]] = i
-
-
-print "loading word vector model"
-
-wordvec_model = gensim.models.KeyedVectors.load_word2vec_format('./model/GoogleNews-vectors-negative300.bin', binary=True)  
-
-
-print len(vocab)
-
-print "loading data\r"
-
-train_pos = load_dir(train_pos_dir, 10000)
-print "loaded %i positive training examples\r"%len(train_pos)
-
-train_neg = load_dir(train_neg_dir, 10000)
-print "loaded %i negative training examples\r"%len(train_neg)
-
-test_pos = load_dir(test_pos_dir, 1000)
-print "loaded %i positive test examples\r"%len(test_pos)
-
-test_neg = load_dir(test_neg_dir, 1000)
-print "loaded %i negative test examples\r"%len(test_neg)
-
-sequence_length = max(len(x) for x in train_pos + train_neg + test_pos + test_neg)
-
-print "Seq len", sequence_length
-
-print [w for w in train_pos + train_neg + test_pos + test_neg if len(w) > 30]
-
-# print train_pos[np.argmax([len(sentence) for sentence in train_pos])]
-# print train_neg[np.argmax([len(sentence) for sentence in train_neg])]
-# print test_pos[np.argmax([len(sentence) for sentence in test_pos])]
-# print test_neg[np.argmax([len(sentence) for sentence in test_neg])]
-
-
-
-print "Max length: %u"%sequence_length
-
-print "building training data"
-
-def index(word):
-    if word in vocab_inv:
-        return vocab_inv[word]
-    if word[0].isdigit():
-        return vocab_inv["<NUM>"] #Just a random idea I had that numbers are important to distinguish from words
-    return vocab_inv["<UNK>"]
-    
 
 def build_data(x_pos, x_neg):
     # print [[word for word in sentence if word not in wordvec_model.wv] for sentence in x_pos]
 
     print "    converting to vectors"
 
-    x_pos = [np.array([wordvec_model.wv[word] for word in sentence if word in wordvec_model.wv])
+    x_pos = [[wordvec_model.wv[word] for word in sentence if word in wordvec_model.wv]
                             for sentence in (x_pos)]
-
-    x_neg = [np.array([wordvec_model.wv[word] for word in sentence if word in wordvec_model.wv])
+    x_neg = [[wordvec_model.wv[word] for word in sentence if word in wordvec_model.wv]
                             for sentence in (x_neg)]
 
-    # print len(x_pos)
-    # print [i for i in xrange(len(x_pos)) if x_pos[i].shape[0] == 0]
-    x_pos = [s for s in x_pos if s.shape[0] != 0] # Let's just remove examples with no recognized words
-    # print len(x_pos)
-
-    x_neg = [s for s in x_neg if s.shape[0] != 0] # Let's just remove examples with no recognized words
+    x_pos = [s for s in x_pos if len(s) != 0] # Let's just remove examples with no recognized words
+    x_neg = [s for s in x_neg if len(s) != 0]
 
     num_pos = len(x_pos)
     num_neg = len(x_neg)
@@ -159,38 +101,87 @@ def build_data(x_pos, x_neg):
     del x_pos
     del x_neg
 
-    # print x[0].shape
-
     print "    padding"
-
-
     for i in xrange(len(x)):
-        # print x[i].shape
+        x[i] = np.array(x[i])
         if x[i].ndim == 1:
             x[i] = x[i][:,None]
-        x[i] = np.pad(x[i], [(0, sequence_length - x[i].shape[0]), (0, 0)], "mean")
+        x[i] = np.pad(x[i], [(0, sequence_length - x[i].shape[0]), (0, 0)], "constant")
     x = np.array(x)
 
-
-    # print x[0].shape
-    # print x.shape
-
     print "    generating labels"
-
-
     y = np.concatenate(([[0, 1] for _ in xrange(num_pos)], [[1, 0] for _ in xrange(num_neg)]))
     return (x, y)
 
-x_train, y_train = build_data(train_pos, train_neg)
+# print "loading vocabulary\r"
+# vocab = open(vocab_path).read().splitlines()
 
-# print x_train.shape
-# print y_train.shape
 
-# print x_train.shape
-# print x_train[0]
-print "building test data"
+# # Add various important numbers and tokens
+# vocab+= ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "<NUM>", "<UNK>", "<PAD>"]
+# vocab_inv = {} # Converts words to indexes
+# for i in xrange(len(vocab)):
+#     vocab_inv[vocab[i]] = i
 
-x_test, y_test = build_data(test_pos, test_neg)
+
+print "loading word vector model"
+
+wordvec_model = gensim.models.KeyedVectors.load_word2vec_format('./model/GoogleNews-vectors-negative300.bin', binary=True)  
+
+
+if not FLAGS.skip_train:
+
+    # print len(vocab)
+
+    print "loading data\r"
+
+    train_pos = load_dir(train_pos_dir, 100000)
+    print "loaded %i positive training examples\r"%len(train_pos)
+
+    train_neg = load_dir(train_neg_dir, 100000)
+    print "loaded %i negative training examples\r"%len(train_neg)
+
+    test_pos = load_dir(test_pos_dir, 3000)
+    print "loaded %i positive test examples\r"%len(test_pos)
+
+    test_neg = load_dir(test_neg_dir, 3000)
+    print "loaded %i negative test examples\r"%len(test_neg)
+
+    sequence_length = max(len(x) for x in train_pos + train_neg + test_pos + test_neg)
+
+    print "Seq len", sequence_length
+
+
+    # print train_pos[np.argmax([len(sentence) for sentence in train_pos])]
+    # print train_neg[np.argmax([len(sentence) for sentence in train_neg])]
+    # print test_pos[np.argmax([len(sentence) for sentence in test_pos])]
+    # print test_neg[np.argmax([len(sentence) for sentence in test_neg])]
+
+
+
+    print "Max length: %u"%sequence_length
+
+    print "building training data"
+
+    # def index(word):
+    #     if word in vocab_inv:
+    #         return vocab_inv[word]
+    #     if word[0].isdigit():
+    #         return vocab_inv["<NUM>"] #Just a random idea I had that numbers are important to distinguish from words
+    #     return vocab_inv["<UNK>"]
+        
+
+
+    x_train, y_train = build_data(train_pos, train_neg)
+
+    # print x_train.shape
+    # print y_train.shape
+
+    # print x_train.shape
+    # print x_train[0]
+    print "building test data"
+
+    x_test, y_test = build_data(test_pos, test_neg)
 
 #data_chkpt = open("data.npz", "w")
 
@@ -202,15 +193,19 @@ checkpoint_freq = 1
 embedding_size = 300
 num_filters = 128
 epochs = 50
+learning_rate = 1e-4
+reg = 1e-4
+training_dropout = 0.5
 
 
-sequence_length = x_train.shape[1]
-num_classes = y_train.shape[1]
+sequence_length = FLAGS.max_input_len
+num_classes = 2
 
-vocab_size = len(vocab)
+# vocab_size = len(vocab)
 filter_sizes = map(int, '3,4,5'.split(','))
-validate_every = len(y_train) / (batch_size * valid_freq)
-checkpoint_every = len(y_train) / (batch_size * checkpoint_freq)
+if not FLAGS.skip_train:
+    validate_every = len(y_train) / (batch_size * valid_freq)
+    checkpoint_every = len(y_train) / (batch_size * checkpoint_freq)
 
 device = '/cpu:0'
 if FLAGS.gpu:
@@ -244,7 +239,7 @@ def bias_variable(shape, name):
 
 with tf.device(device):
     # Placeholders
-    data_in = tf.placeholder(tf.int32, [None, sequence_length, embedding_size], name='data_in')
+    data_in = tf.placeholder(tf.float32, [None, sequence_length, embedding_size], name='data_in')
     data_out = tf.placeholder(tf.float32, [None, num_classes], name='data_out')
     dropout_keep_prob = tf.placeholder(tf.float32, name='dropout_keep_prob')
     # Stores the accuracy of the model for each batch of the validation testing
@@ -260,7 +255,8 @@ with tf.device(device):
     #                                       -1.0, 1.0),
     #                     name='embedding_matrix')
     #     embedded_chars = tf.nn.embedding_lookup(W, data_in)
-    #     embedded_chars_expanded = tf.expand_dims(embedded_chars, -1)
+            # W = weight_variable(filter_shape, name='W_conv')
+    embedded_chars_expanded = tf.expand_dims(data_in, -1)
 
     # Convolution + ReLU + Pooling layer
     pooled_outputs = []
@@ -273,7 +269,7 @@ with tf.device(device):
                             num_filters]
             W = weight_variable(filter_shape, name='W_conv')
             b = bias_variable([num_filters], name='b_conv')
-            conv = tf.nn.conv2d(data_in,
+            conv = tf.nn.conv2d(embedded_chars_expanded,
                                 W,
                                 strides=[1, 1, 1, 1],
                                 padding='VALID',
@@ -308,10 +304,12 @@ with tf.device(device):
         network_out = tf.nn.softmax(tf.matmul(h_drop, W_out) + b_out)
 
     # Loss function
-    cross_entropy = -tf.reduce_sum(data_out * tf.log(network_out))
+    regularizer = tf.nn.l2_loss(W_out) + tf.nn.l2_loss(W)
+    cross_entropy = -tf.reduce_sum(data_out * tf.log(network_out)) 
+    loss = cross_entropy + reg*regularizer
 
     # Training algorithm
-    train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+    train_step = tf.train.AdamOptimizer(learning_rate).minimize(loss)
 
     # Testing operations
     correct_prediction = tf.equal(tf.argmax(network_out, 1),
@@ -321,6 +319,9 @@ with tf.device(device):
     # Validation ops
     valid_mean_accuracy = tf.reduce_mean(valid_accuracies)
     valid_mean_loss = tf.reduce_mean(valid_losses)
+    
+    grad = tf.gradients(cross_entropy, data_in) # behold the power of automatic differentiation!
+
 
 
 
@@ -391,7 +392,7 @@ if not FLAGS.skip_train:
         # Run the training step
         feed_dict = {data_in: x_batch,
                      data_out: y_batch,
-                     dropout_keep_prob: 0.5}
+                     dropout_keep_prob: 1.0 - training_dropout}
         train_result, loss_summary_result = sess.run([train_step, loss_summary],
                                                      feed_dict=feed_dict)
 
@@ -489,8 +490,13 @@ def evaluate_sentence(sentence):
     if len(sentence) > sequence_length:
         print "sentence too long"
         return
-    x_to_eval = np.array([index(word) for word in sentence] + [vocab_inv["<PAD>"]]*(sequence_length - len(sentence)))[None,:]
 
+
+
+    x_to_eval = np.array([wordvec_model.wv[word] for word in sentence if word in wordvec_model.wv])
+
+
+    x_to_eval = np.array([np.pad(x_to_eval, [(0, sequence_length - x_to_eval.shape[0]), (0, 0)], "constant")])
 
     # x_to_eval = string_to_int(sentence, vocabulary, max(len(_) for _ in x))
     result = sess.run(tf.argmax(network_out, 1),
@@ -503,30 +509,41 @@ def evaluate_sentence(sentence):
     #log('Custom input evaluation:', network_sentiment)
     #log('Actual output:', str(unnorm_result[0]))
 
-def get_word_grads(sentence, vocabulary, target):
+def get_word_grads(sentence, target):
     """
     Translates a string to its equivalent in the integer vocabulary and feeds it
     to the network.
     Outputs result to stdout.
     """
-    x_to_eval = string_to_int(sentence, vocabulary, max(len(_) for _ in x))
-    feed_dict = {data_in: x_to_eval,
-                 data_out: target,
-                 dropout_keep_prob: 1.0}
-
-    grad = tf.gradients(accuracy, embedded_chars) # behold the power of automatic differentiation!
+    sentence = sentence.strip().lower().translate(None, string.punctuation).split(" ")
+    if len(sentence) > sequence_length:
+        print "sentence too long"
+        return
 
 
 
-    result = sess.run(tf.argmax(network_out, 1),
-                      feed_dict={data_in: x_to_eval,
-                                 dropout_keep_prob: 1.0})
-    unnorm_result = sess.run(network_out, feed_dict={data_in: feed_dict,
-                                                     dropout_keep_prob: 1.0})
-    network_sentiment = 'POS' if result == 1 else 'NEG'
-    print network_sentiment, unnorm_result
+    x_to_eval = np.array([wordvec_model.wv[word] for word in sentence if word in wordvec_model.wv])
+    x_to_eval = np.array([np.pad(x_to_eval, [(0, sequence_length - x_to_eval.shape[0]), (0, 0)], "constant")])
+
+    y_target = np.array([[0.5, 0.5]]) 
+
+    if target > 0:
+        y_target = np.array([[0, 1]]) 
+    elif target < 0:
+        y_target = np.array([[1, 0]]) 
+
+
+    # x_to_eval = string_to_int(sentence, vocabulary, max(len(_) for _ in x))
+
+    gradients = sess.run(grad, feed_dict={data_in: x_to_eval, data_out: y_target, dropout_keep_prob: 1.0})
+    print gradients
     #log('Custom input evaluation:', network_sentiment)
     #log('Actual output:', str(unnorm_result[0]))
 
+
 if FLAGS.custom_input != '':
-    evaluate_sentence(FLAGS.custom_input)
+    if FLAGS.get_grads != 0:
+        get_word_grads(FLAGS.custom_input, FLAGS.get_grads)
+    else:
+        evaluate_sentence(FLAGS.custom_input)
+